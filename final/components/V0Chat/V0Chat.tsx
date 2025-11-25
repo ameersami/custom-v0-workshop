7e0ec6b082b2c7ef90cd3d9125c63b7b13361483
Ameer Sami <ameer.sami314@gmail.com>
2025-11-25 01:27:50 -0500

    Updated the V0Chat component to address linting issues

--- final/components/V0Chat/V0Chat.tsx
+++ final/components/V0Chat/V0Chat.tsx
@@ -23,7 +23,7 @@ interface ChatHistory {
   componentTextString?: string;
 }
 
-export default ({ handleModel }: v0ChatProps) => {
+const V0Chat = ({ handleModel }: v0ChatProps) => {
 
   const [prompt, promptSetter] = useState('');
   const [chatHistory, chatHistorySetter] = useState<Array<ChatHistory>>([]);
@@ -37,8 +37,8 @@ export default ({ handleModel }: v0ChatProps) => {
     if (chatRef.current) {
       const config = { childList: true };
   
-      const callback = (mutationsList: MutationRecord[], observer: MutationObserver) => {
-        for (let mutation of mutationsList) {
+      const callback = (mutationsList: MutationRecord[]) => {
+        for (const mutation of mutationsList) {
           if (mutation.type === "childList" && chatRef.current) {
             chatRef.current.scrollTo(0, chatRef.current.scrollHeight);
           }
@@ -132,7 +132,7 @@ export default ({ handleModel }: v0ChatProps) => {
                     role="button"
                     onClick={() => handleDownloadGeneratedCode(message?.componentTextString ?? '')}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><title>file-download</title><g fill="oklch(62.7% 0.265 303.9)"><path fill-rule="evenodd" clip-rule="evenodd" d="M20.2426 2C19.1818 2 18.1644 2.42143 17.4142 3.17157L7.17157 13.4142C6.42153 14.1643 6 15.1812 6 16.2422V40C6 43.3137 8.68629 46 12 46H25.3253C22.967 43.7264 21.5 40.5345 21.5 37C21.5 30.0964 27.0964 24.5 34 24.5C37.0431 24.5 39.8322 25.5874 42 27.3949V7.9991C42 4.68516 39.3135 2 36 2H20.2426ZM20 16V5L9 16H20Z" fill="oklch(62.7% 0.265 303.9)"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M43.5 37C43.5 42.2467 39.2467 46.5 34 46.5C28.7533 46.5 24.5 42.2467 24.5 37C24.5 31.7533 28.7533 27.5 34 27.5C39.2467 27.5 43.5 31.7533 43.5 37ZM35.5 37.3787L37.6449 35.2338L39.7662 37.3551L34.0001 43.1213L28.2306 37.3518L30.352 35.2305L32.5 37.3786V32H35.5V37.3787Z" fill="oklch(62.7% 0.265 303.9)"></path></g></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"><title>file-download</title><g fill="oklch(62.7% 0.265 303.9)"><path fillRule="evenodd" clipRule="evenodd" d="M20.2426 2C19.1818 2 18.1644 2.42143 17.4142 3.17157L7.17157 13.4142C6.42153 14.1643 6 15.1812 6 16.2422V40C6 43.3137 8.68629 46 12 46H25.3253C22.967 43.7264 21.5 40.5345 21.5 37C21.5 30.0964 27.0964 24.5 34 24.5C37.0431 24.5 39.8322 25.5874 42 27.3949V7.9991C42 4.68516 39.3135 2 36 2H20.2426ZM20 16V5L9 16H20Z" fill="oklch(62.7% 0.265 303.9)"></path> <path fillRule="evenodd" clipRule="evenodd" d="M43.5 37C43.5 42.2467 39.2467 46.5 34 46.5C28.7533 46.5 24.5 42.2467 24.5 37C24.5 31.7533 28.7533 27.5 34 27.5C39.2467 27.5 43.5 31.7533 43.5 37ZM35.5 37.3787L37.6449 35.2338L39.7662 37.3551L34.0001 43.1213L28.2306 37.3518L30.352 35.2305L32.5 37.3786V32H35.5V37.3787Z" fill="oklch(62.7% 0.265 303.9)"></path></g></svg>
                   </button>
                   <GeneratedCodeRenderer>
                     {message.componentContent}
@@ -169,3 +169,5 @@ export default ({ handleModel }: v0ChatProps) => {
   )
 
 };
+
+export default V0Chat;
